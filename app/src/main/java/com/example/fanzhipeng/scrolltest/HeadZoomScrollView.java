7a034704a1cccb314204de637abd15de03c708d9
范志鹏 <fanzhipeng@fanzhipengdeMacBook-Pro.local>
2017-11-24 23:04:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fanzhipeng/scrolltest/HeadZoomScrollView.java
@@ -0,0 +1,239 @@
+package com.example.fanzhipeng.scrolltest;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+
+/**
+ * Created by fanzhipeng on 2017/11/24.
+ */
+public class HeadZoomScrollView extends ScrollView {
+
+    public HeadZoomScrollView(Context context) {
+        super(context);
+    }
+
+    public HeadZoomScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HeadZoomScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    //    用于记录下拉位置
+    private float y = 0f;
+    //    zoomView原本的宽高
+    private int zoomViewWidth = 0;
+    private int zoomViewHeight = 0;
+
+    //    是否正在放大
+    private boolean mScaling = false;
+
+    //    放大的view，默认为第一个子view
+    private View zoomView;
+
+    //移动因子, 是一个百分比, 比如手指移动了100px, 那么View就只移动50px
+    //目的是达到一个延迟的效果
+    private static final float MOVE_FACTOR = 0.5f;
+
+    //松开手指后, 界面回到正常位置需要的动画时间
+    private static final int ANIM_TIME = 300;
+
+    //ScrollView的子View， 也是ScrollView的唯一一个子View
+    private View contentView;
+
+    //手指按下时的Y值, 用于在移动时计算移动距离
+    //如果按下时不能上拉和下拉， 会在手指移动时更新为当前手指的Y值
+    private float startY;
+
+    //用于记录正常的布局位置
+    private Rect originalRect = new Rect();
+
+    //手指按下时记录是否可以继续上拉
+    private boolean canPullUp = false;
+
+    //在手指滑动的过程中记录是否移动了布局
+    private boolean isMoved = false;
+
+    public void setZoomView(View zoomView) {
+        this.zoomView = zoomView;
+    }
+
+    //    滑动放大系数，系数越大，滑动时放大程度越大
+    private float mScaleRatio = 0.4f;
+
+    public void setmScaleRatio(float mScaleRatio) {
+        this.mScaleRatio = mScaleRatio;
+    }
+
+    //    最大的放大倍数
+    private float mScaleTimes = 2f;
+
+    public void setmScaleTimes(int mScaleTimes) {
+        this.mScaleTimes = mScaleTimes;
+    }
+
+    //    回弹时间系数，系数越小，回弹越快
+    private float mReplyRatio = 0.5f;
+
+    public void setmReplyRatio(float mReplyRatio) {
+        this.mReplyRatio = mReplyRatio;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+//        不可过度滚动，否则上移后下拉会出现部分空白的情况
+        setOverScrollMode(OVER_SCROLL_NEVER);
+//        获得默认第一个view
+        if (getChildAt(0) != null && getChildAt(0) instanceof ViewGroup && zoomView == null) {
+            ViewGroup vg = (ViewGroup) getChildAt(0);
+            if (vg.getChildCount() > 0) {
+                zoomView = vg.getChildAt(0);
+            }
+        }
+        if (getChildCount() > 0) {
+            contentView = getChildAt(0);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+
+        if (contentView == null) return;
+
+        //ScrollView中的唯一子控件的位置信息, 这个位置信息在整个控件的生命周期中保持不变
+        originalRect.set(contentView.getLeft(), contentView.getTop(), contentView
+                .getRight(), contentView.getBottom());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (zoomViewWidth <= 0 || zoomViewHeight <= 0) {
+            zoomViewWidth = zoomView.getMeasuredWidth();
+            zoomViewHeight = zoomView.getMeasuredHeight();
+        }
+        if (zoomView == null || zoomViewWidth <= 0 || zoomViewHeight <= 0) {
+            return super.onTouchEvent(ev);
+        }
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                canPullUp = isCanPullUp();
+
+                //记录按下时的Y值
+                startY = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //在移动的过程中， 既没有滚动到可以上拉的程度， 也没有滚动到可以下拉的程度
+                if (!canPullUp) {
+                    startY = ev.getY();
+                    //    canPullDown = isCanPullDown();
+                    canPullUp = isCanPullUp();
+                }
+
+                //计算手指移动的距离
+                float nowY = ev.getY();
+                int deltaY = (int) (nowY - startY);
+
+                //是否应该移动布局
+                boolean shouldMove = (canPullUp && deltaY < 0);
+
+                if (shouldMove) {
+                    //计算偏移量
+                    int offset = (int) (deltaY * MOVE_FACTOR);
+
+                    //随着手指的移动而移动布局
+                    contentView.layout(originalRect.left, originalRect.top + offset,
+                            originalRect.right, originalRect.bottom + offset);
+
+                    isMoved = true;  //记录移动了布局
+                }
+                if (!mScaling) {
+                    if (getScrollY() == 0) {
+                        y = ev.getY();//滑动到顶部时，记录位置
+                    } else {
+                        break;
+                    }
+                }
+                int distance = (int) ((ev.getY() - y) * mScaleRatio);
+                if (distance < 0) break;//若往下滑动
+                mScaling = true;
+                setZoom(distance);
+                return true;
+            case MotionEvent.ACTION_UP:
+                mScaling = false;
+                replyView();
+                break;
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    /**
+     * 放大view
+     */
+    private void setZoom(float s) {
+        float scaleTimes = (float) ((zoomViewWidth + s) / (zoomViewWidth * 1.0));
+//        如超过最大放大倍数，直接返回
+        if (scaleTimes > mScaleTimes) return;
+
+        ViewGroup.LayoutParams layoutParams = zoomView.getLayoutParams();
+        layoutParams.width = (int) (zoomViewWidth + s);
+        layoutParams.height = (int) (zoomViewHeight * ((zoomViewWidth + s) / zoomViewWidth));
+//        设置控件水平居中
+        ((MarginLayoutParams) layoutParams).setMargins(-(layoutParams.width - zoomViewWidth) / 2, 0, 0, 0);
+        zoomView.setLayoutParams(layoutParams);
+    }
+
+    /**
+     * 回弹
+     */
+    private void replyView() {
+        final float distance = zoomView.getMeasuredWidth() - zoomViewWidth;
+        // 设置动画
+        ValueAnimator anim = ObjectAnimator.ofFloat(distance, 0.0F).setDuration((long) (distance * mReplyRatio));
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                setZoom((Float) animation.getAnimatedValue());
+            }
+        });
+        anim.start();
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        if (onScrollListener != null) onScrollListener.onScroll(l, t, oldl, oldt);
+    }
+
+    private OnScrollListener onScrollListener;
+
+    public void setOnScrollListener(OnScrollListener onScrollListener) {
+        this.onScrollListener = onScrollListener;
+    }
+
+    /**
+     * 滑动监听
+     */
+    public interface OnScrollListener {
+        void onScroll(int scrollX, int scrollY, int oldScrollX, int oldScrollY);
+    }
+
+
+    /**
+     * 判断是否滚动到底部
+     */
+    private boolean isCanPullUp() {
+        return contentView.getHeight() <= getHeight() + getScrollY();
+    }
+
+
+}
